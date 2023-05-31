d7bec77376edec2e31c3c63c702be518233fe3e8
mat-jar <mtu07@poczta.fm>
2023-06-01 00:36:24 +0200

    Add location from IP-geolocation-API to weather-API

--- app/services/weather_api_connector.rb
+++ app/services/weather_api_connector.rb
@@ -3,8 +3,9 @@ class WeatherApiConnector
 API_KEY = A9n.weather_api_key
 LOCATION = 'Cracow'
 
-  def weather_data
-    url = "http://api.weatherapi.com/v1/current.json?key=#{API_KEY}&q=#{LOCATION}"
+  def weather_data(ip_address)
+    location = IpgeolocationApiConnector.new.find_location(ip_address) || LOCATION
+    url = "http://api.weatherapi.com/v1/current.json?key=#{API_KEY}&q=#{location}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     JSON.parse(response)
