1878fad03c248b4f3c26f24755cdbb8379742b5c
mat-jar <mtu07@poczta.fm>
2023-06-04 16:48:27 +0200

    Fix DueDateNotificationJob#perform to include loans due_date tomorrow

--- app/jobs/due_date_notification_job.rb
+++ app/jobs/due_date_notification_job.rb
@@ -2,7 +2,7 @@ class DueDateNotificationJob
   include Sidekiq::Job
 
   def perform
-    BookLoan.where(due_date: Date.tomorrow, status: 'checked_out').each do |book_loan|
+    BookLoan.where(:due_date => (Date.tomorrow.beginning_of_day)..(Date.tomorrow.end_of_day), status: 'checked_out').each do |book_loan|
       UserMailer.due_date_notification_email(book_loan).deliver_now
     end
   end
